9995c5a31db7d0a2dcb7ef16a2d3b7bab5cf74e2
jesus <jesus.gareca@edu.uah.es>
2025-04-02 08:48:13 +0200

    Base de SuperArbol

---
+++ src/ArbolBinarioDeBusqueda.java
@@ -0,0 +1,46 @@
+public class ArbolBinarioDeBusqueda {
+
+    //Listas Generadas
+    public getListaPreOrden(){
+        return list
+    }
+    public getListaPostOrden(){
+        return list
+    }
+    public getListaOrdenCentral(){
+        return list
+    }
+
+    //Operaciones
+    public void add(){
+    }
+    public Lista<T> getSubArbolIzquierda(){
+        return ArbolBinarioDeBusqueda
+    }
+    public Lista<T> getSubArbolDerecha(){
+        return ArbolBinarioDeBusqueda
+    }
+
+    //Responder preguntas
+    public int getGrado(){
+        return int
+    }
+    public int getAltura(){
+        return int
+    }
+    public Lista<T> getListaDatosNivel(nivel){
+        return list
+    }
+    public boolean isArbolHomogeneo(){
+        return boolean
+    }
+    public boolean isArbolCompleto(){
+        return boolean
+    }
+    public boolean isArbolCasiCompleto(){
+        return boolean
+    }
+    public Lista<T> getCamino(){
+        return list
+    }
+}
---
+++ src/ArbolBinarioDeBusquedaEnteros.java
@@ -0,0 +1,9 @@
+public class ArbolBinarioDeBusquedaEnteros<T> implements ArbolBinarioDeBusqueda {
+    //Datos
+
+    //Funciones
+    public getSuma(){ //Obtener la suma de todos los elementos del elemento
+        return int
+    }
+
+}
